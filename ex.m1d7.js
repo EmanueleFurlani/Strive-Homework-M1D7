46aadf230d8b5e954d6f98c801f720a05632d1ca
EmanueleFurlani <emanuelefurlani.go@gmail.com>
2021-06-29 16:42:26 +0200

    Ex 1+2+3

---
+++ ex.m1d7.js
@@ -0,0 +1,8 @@
+let changeOneElement = function() {
+    let title = document.getElementsByTagName("h1")
+    title.classList.add("red")
+}
+let changeBg = function() {
+    let bg = document.getElementsByTagName("body")
+    bg.classList("black")
+}
